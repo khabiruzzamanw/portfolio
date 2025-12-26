b67763eb5ee8ebe051925234ac490ed775155c83
Khabiruzzaman Onwork <khabiruzzamanonwork@gmail.com>
2025-12-27 01:28:56 +0600

    m

---
+++ theme.js
@@ -0,0 +1,21 @@
+const theme = document.getElementById("theme");
+let imgSrc = theme.src;
+
+theme.addEventListener("mouseover", () => {
+  theme.style.cursor = "pointer";
+});
+
+theme.addEventListener("click", () => {
+  // document.body.classList.toggle("lightMode");
+  let themeData = document.body.getAttribute("class");
+
+  if (themeData === "dark") {
+    document.body.setAttribute("class", "light");
+    theme.src = "/images/lightMode.svg";
+  } else {
+    document.body.setAttribute("class", "dark");
+    theme.src = "/images/darkMode.svg";
+  }
+
+  console.log(document.body.classList);
+});
