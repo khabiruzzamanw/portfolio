8ae1cf32f619c3bb60fbadce9c55028b47245e4b
khabiruzzamanW <khabiruzzamanonwork@gmail.com>
2026-01-22 22:30:07 +0600

    added files to the local git user

---
+++ index.js
@@ -0,0 +1,29 @@
+
+
+
+//this section is for the theme changer
+
+const theme = document.getElementById("theme");
+let imgSrc = theme.src;
+
+theme.addEventListener("mouseover", () => {
+  theme.style.cursor = "pointer";
+});
+
+theme.addEventListener("click", () => {
+  // document.body.classList.toggle("lightMode");
+  let themeData = document.body.getAttribute("class");
+
+  if (themeData === "dark") {
+    // document.body.removeAttribute("class", "dark");
+    document.body.setAttribute("class", "light");
+    theme.src = "images/lightMode.svg";
+  } else {
+    document.body.setAttribute("class", "dark");
+    theme.src = "images/darkMode.svg";
+  }
+
+  console.log(document.body.classList);
+});
+
+//-----------------------------------------------------------//---------------------------------------------------------------//
