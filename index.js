8469f9103589d09d67a9a65a7b90ef851332c709
khabiruzzamanW <khabiruzzamanonwork@gmail.com>
2026-01-23 20:35:12 +0600

    modified files

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ theme.addEventListener("click", () => {
 
   if (themeData.contains('dark')) {
     // document.body.removeAttribute("class", "dark");
-    themeData.replace('dark','light');
+    themeData.replace('dark', 'light');
     theme.src = "images/lightMode.svg";
 
   } else {
-    themeData.replace('light','dark');
+    themeData.replace('light', 'dark');
     theme.src = "images/darkMode.svg";
   }
 
@@ -28,3 +28,66 @@ theme.addEventListener("click", () => {
 
 
 //-----------------------------------------------------------//---------------------------------------------------------------//
+
+
+//===========================================================this section is for links==========================================================================
+
+const github = document.querySelector('#github');
+
+github.href = "https://github.com/khabiruzzaman-codes";
+
+const linkedin = document.querySelector('#linkedin');
+
+linkedin.href = "https://www.linkedin.com/in/khabiruzzaman-codes";
+
+const twitter = document.querySelector('#twitter');
+
+twitter.href = "https://x.com/khabiruzzaman_c";
+
+const instagram = document.querySelector('#instagram');
+
+instagram.href = "https://www.instagram.com/khabiruzzaman.codes";
+
+const youtube = document.querySelector('#youtube');
+
+youtube.href = "https://www.youtube.com/@khabiruzzaman.codes";
+
+const email = document.querySelector('#email');
+
+email.href = "khabiruzzaman.codes@gmail.com";
+
+
+
+const githubWeatherCheckerRepo = document.querySelector("#githubWeatherCheckerRepo");
+
+githubWeatherCheckerRepo.href = "https://github.com/khabiruzzamanw/weatherChecker";
+
+
+const githubJournixRepo = document.querySelector("#githubJournixRepo");
+
+githubJournixRepo.href = "https://github.com/khabiruzzamanw/journix";
+
+
+const githubGuessTheNumberRepo = document.querySelector("#githubGuessTheNumberRepo");
+
+githubGuessTheNumberRepo.href = "";
+
+
+
+
+const weatherChecker = document.querySelector("#weatherChecker");
+
+weatherChecker.href = "https://khabiruzzamanw.github.io/weatherChecker";
+
+
+const journix = document.querySelector("#journix");
+
+journix.href = "https://khabiruzzamanw.github.io/journix";
+
+
+const guessTheNumber = document.querySelector("#guessTheNumber");
+
+guessTheNumber.href = "";
+
+
+//---------------------------------------------------------------//
